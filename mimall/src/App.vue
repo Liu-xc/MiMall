c11075d55b86414dfc2bc72c42e5e624d6a342ec
Liu-xc <1392166232@qq.com>
2020-06-17 18:49:14 +0800

    Storage & axios vue-axios

--- mimall/src/App.vue
+++ mimall/src/App.vue
@@ -3,5 +3,12 @@
     <router-view />
   </div>
 </template>
-
+<script>
+// import storage from './storage'
+export default {
+  name: 'app',
+  mounted () {
+  },
+}
+</script>
 <style lang="stylus"></style>
